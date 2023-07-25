aee0d5e8a42d66a0308126b404c9c5420a4d44c5
Nikhil Patil <patilnikhil1033@gmail.com>
2023-07-25 12:45:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import Header from "./components/Header";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+
+function App() {
+  return (
+    <div>
+    <h2 className="project">Shopping Cart Application </h2>
+    <BrowserRouter>
+      <Header />
+      <div className="App">
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+      </div>
+    </BrowserRouter>
+    </div>
+  );
+}
+
+
+export default App;
